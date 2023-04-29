bd818ab3a54d70b38038636cef370154a6a601e4
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-04-29 21:08:47 +0200

    Creating basic function js

--- ColorFliper/colorChanging.js
+++ ColorFliper/colorChanging.js
@@ -1,1 +1,12 @@
-const colors = ["green", "red", "#f15025", "rgba(133,122,200)"]
+const colors = ["green", "red", "#f15025", "rgba(133,122,200)"];
+
+const button = document.getElementById('button');
+const colorSpan = document.querySelector('color');
+
+button.addEventListener('click', function(){
+    //console.log(document.body);
+    //Getting random number between 0 - 3
+    const randomNum = 2; 
+    document.body.style.backgroundColor = colors[randomNum];
+    colorSpan.textContent = colors[randomNum];
+})

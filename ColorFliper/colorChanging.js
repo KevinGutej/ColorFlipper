332db1eb25d5864ecf8f6f3e949732ab572fed40
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-04-29 21:17:56 +0200

    Using functions like math.floor to round up our randomized number

--- ColorFliper/colorChanging.js
+++ ColorFliper/colorChanging.js
@@ -1,12 +1,37 @@
-const colors = ["green", "red", "#f15025", "rgba(133,122,200)"];
+const colors = [
+    "#FF6633",
+    "#FFB399",
+    "#FF33FF",
+    "#FFFF99",
+    "#00B3E6",
+    "#E6B333",
+    "#3366E6",
+    "#999966",
+    "#809980",
+    "#E6FF80",
+    "#1AFF33",
+    "#999933",
+    "#FF3380",
+    "#CCCC00",
+    "#66E64D",
+    "#4D80CC",
+    "#FF4D4D",
+    "#99E6E6",
+    "#6666FF"
+];
 
 const button = document.getElementById('button');
-const colorSpan = document.querySelector('color');
+const colorSpan = document.querySelector('.color');
 
-button.addEventListener('click', function(){
-    //console.log(document.body);
-    //Getting random number between 0 - 3
-    const randomNum = 2; 
+button.addEventListener("click", function () {
+    //Getting random number between 0-3
+    const randomNum = getRandomNum(); 
+    console.log(randomNum);
+
     document.body.style.backgroundColor = colors[randomNum];
     colorSpan.textContent = colors[randomNum];
-})
+});
+
+function getRandomNum() {
+    return Math.floor(Math.random() * colors.length); //Here using the Math.floor When we randomize a number it will round to the nearest one so e.g 0.9 = 0 ; and thats how we get random number in this case from 0 - 3
+}
